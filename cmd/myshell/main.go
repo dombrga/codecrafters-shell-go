c7f391c9832acba930e7cadd9ecdd796741c5f88
dom <dom.barga@gmail.com>
2025-02-12 23:31:15 +0800

    print command not found

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -14,5 +14,7 @@ func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	var input, _ = bufio.NewReader(os.Stdin).ReadString('\n')
+
+	fmt.Printf("%s: command not found", input[:len(input)-1])
 }
