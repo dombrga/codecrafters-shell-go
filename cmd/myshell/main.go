49f2a9fe658abaa7f9de174bee7c98c8e6c661ec
dom <dom.barga@gmail.com>
2025-02-12 23:59:53 +0800

    added exit

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -6,8 +6,7 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Fprint
+var exitCommand = "exit"
 
 func main() {
 
@@ -16,12 +15,22 @@ func main() {
 		fmt.Fprint(os.Stdout, "$ ")
 
 		// Wait for user input
-		var input, err = bufio.NewReader(os.Stdin).ReadString('\n')
+		var c, err = bufio.NewReader(os.Stdin).ReadString('\n')
 
 		if err != nil {
 			fmt.Println("error reading from standard input: ", err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s: command not found\n", input[:len(input)-1])
+		// fmt.Println("inout c", c)
+
+		// without the \n
+		// var commands = strings.Split(c, " ")
+		var command = c[:len(c)-1]
+		if command == (exitCommand + " 0") {
+			// var firstArg = os.Args[0]
+			// fmt.Println("first arg", firstArg)
+			os.Exit(0)
+		}
+		fmt.Printf("%s: command not found\n", command[:len(command)-1])
 	}
 }
