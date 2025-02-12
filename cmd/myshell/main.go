bdaac1b043d23fc1ab77cecfd4f2f58e36004ba1
dom <dom.barga@gmail.com>
2025-02-12 23:41:51 +0800

    added repl loop

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,16 +10,18 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	var input, err = bufio.NewReader(os.Stdin).ReadString('\n')
+	for {
+		// Uncomment this block to pass the first stage
+		fmt.Fprint(os.Stdout, "$ ")
 
-	if err != nil {
-		fmt.Println("error reading from standard input: ", err)
-		os.Exit(1)
+		// Wait for user input
+		var input, err = bufio.NewReader(os.Stdin).ReadString('\n')
+
+		if err != nil {
+			fmt.Println("error reading from standard input: ", err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s: command not found\n", input[:len(input)-1])
 	}
-
-	fmt.Printf("%s: command not found", input[:len(input)-1])
 }
