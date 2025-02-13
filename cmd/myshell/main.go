0afa55bf06bb916d44e18f1827937b6f8b2f85dc
dom <dom.barga@gmail.com>
2025-02-13 22:45:01 +0800

    added type command

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -7,11 +7,17 @@ import (
 	"strings"
 )
 
+var exitCommand = "exit"
 var exit0Command = "exit 0"
 var echoCommand = "echo"
+var typeCommand = "type"
+var cmds = map[string]string{
+	echoCommand: echoCommand,
+	exitCommand: exitCommand,
+	typeCommand: typeCommand,
+}
 
 func main() {
-
 	for {
 		// Uncomment this block to pass the first stage
 		fmt.Fprint(os.Stdout, "$ ")
@@ -23,7 +29,6 @@ func main() {
 			fmt.Println("error reading from standard input: ", err)
 			os.Exit(1)
 		}
-		// fmt.Println("inout c", c)
 
 		// without the \n
 		var input = strings.TrimSpace(c)
@@ -31,16 +36,26 @@ func main() {
 		var command = split[0]
 
 		switch command {
-		case "exit":
-			if len(split) > 1 && split[1] == "0" {
-				os.Exit(0)
-			}
+		case "":
+			fmt.Print()
+		case exitCommand:
+			os.Exit(0)
+			// if len(split) > 1 && split[1] == "0" {
+			// 	os.Exit(0)
+			// }
 		case echoCommand:
 			fmt.Fprintln(os.Stdout, strings.Join(split[1:], " "))
+		case typeCommand:
+			if len(split) > 1 {
+				var arg, ok = cmds[split[1]]
+				if ok {
+					fmt.Fprintf(os.Stdout, "%s is a shell builtin\n", arg)
+				} else {
+					fmt.Fprintf(os.Stdout, "%s: command not found\n", split[1])
+				}
+			}
 		default:
-			fmt.Fprintf(os.Stdout, "%s: command not found\n", input)
+			fmt.Fprintf(os.Stdout, "%s: command not found\n", command)
 		}
-
-		// fmt.Printf("%s: command not found\n", input)
 	}
 }
