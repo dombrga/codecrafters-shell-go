d3c20fbcaef2a1fda8000b9d04bf5076c0990eb8
dom <dom.barga@gmail.com>
2025-02-15 00:20:44 +0800

    added type builtin exe files

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -11,13 +11,14 @@ var exitCommand = "exit"
 var exit0Command = "exit 0"
 var echoCommand = "echo"
 var typeCommand = "type"
-var cmds = map[string]string{
+var builtinCmds = map[string]string{
 	echoCommand: echoCommand,
 	exitCommand: exitCommand,
 	typeCommand: typeCommand,
 }
 
 func main() {
+MAIN_LOOP:
 	for {
 		// Uncomment this block to pass the first stage
 		fmt.Fprint(os.Stdout, "$ ")
@@ -47,12 +48,33 @@ func main() {
 			fmt.Fprintln(os.Stdout, strings.Join(split[1:], " "))
 		case typeCommand:
 			if len(split) > 1 {
-				var arg, ok = cmds[split[1]]
+				typeArg := split[1]
+				var biCmd, ok = builtinCmds[typeArg] // biCmd is a builtin command that is the argument of type command
+
 				if ok {
-					fmt.Fprintf(os.Stdout, "%s is a shell builtin\n", arg)
+					fmt.Fprintf(os.Stdout, "%s is a shell builtin\n", biCmd)
 				} else {
-					fmt.Fprintf(os.Stdout, "%s: not found\n", split[1])
+					var _path = os.Getenv("PATH")
+					var path = strings.Split(_path, ":")
+
+					for _, p := range path {
+						ls, err := os.ReadDir(p)
+						if err != nil {
+							fmt.Fprintln(os.Stdout, "error ReadDir:", err)
+						}
+
+						for _, entry := range ls {
+							entryName := entry.Name()
+							if entryName == typeArg {
+								fmt.Fprintf(os.Stdout, "%s is %s/%s\n", typeArg, p, typeArg)
+								continue MAIN_LOOP // once found, don't run below by continuing MAIN_LOOP
+							}
+						}
+					}
+
+					fmt.Fprintf(os.Stdout, "%s: not found\n", typeArg)
 				}
+
 			}
 		default:
 			fmt.Fprintf(os.Stdout, "%s: command not found\n", command)
